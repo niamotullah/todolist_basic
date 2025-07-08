2c85ac7f17fffd36b1f41d4471c65f90a51a6c7d
Niamot <58494481+niamotullah@users.noreply.github.com>
2025-07-09 03:29:48 +0600

    disable DevicePreview on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +10,7 @@ import 'package:todolist_basic/provider/task_list_provider.dart';
 void main() {
   runApp(
     DevicePreview(
-      enabled: !kReleaseMode,
+      enabled: !kReleaseMode && !Platform.isAndroid,
       builder: (context) => TodoApp(),
     ),
   );
