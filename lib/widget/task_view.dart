07f96159ce36ddaab4b4342d39698d1206db73ce
Niamot <58494481+niamotullah@users.noreply.github.com>
2025-07-15 02:17:04 +0600

    update (feature) : applied LineThrough decoration on completed tasks

--- lib/widget/task_view.dart
+++ lib/widget/task_view.dart
@@ -3,23 +3,40 @@ import 'package:provider/provider.dart';
 import 'package:todolist_basic/model/task_model.dart';
 import 'package:todolist_basic/provider/todo_data_provider.dart';
 
-class TaskView extends StatelessWidget {
+class TaskView extends StatefulWidget {
   const TaskView({required this.task, super.key});
   final TodoModel task;
 
+  @override
+  State<TaskView> createState() => _TaskViewState();
+}
+
+class _TaskViewState extends State<TaskView> {
   @override
   Widget build(BuildContext context) {
     return Consumer<TodoDataProvider>(
       builder: (context, taskListProvider, child) {
         return Card(
           child: ListTile(
+            contentPadding: EdgeInsets.all(8),
             leading: Checkbox.adaptive(
-              value: task.isDone,
+              value: widget.task.isDone,
               onChanged: (value) {
-                if (value != null) taskListProvider.toggle(task);
+                if (value != null) taskListProvider.toggle(widget.task);
               },
             ),
-            title: Text(task.title),
+            title: Text(
+              widget.task.title,
+              style:
+                  Theme.of(
+                    context,
+                  ).textTheme.bodyLarge?.copyWith(
+                    fontSize: 17,
+                    decoration: widget.task.isDone
+                        ? TextDecoration.lineThrough
+                        : null,
+                  ),
+            ),
           ),
         );
       },
